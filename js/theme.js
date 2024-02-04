d9fd7e6a4fa6fd137d3e0ef7b9046b3a863d54c8
Nouman <nomi676767@gmail.com>
2024-02-04 20:12:08 +0500

    first commit

---
+++ js/theme.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+	$(".home-slider").owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 30,
+		nav: true,
+		dots: false,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 1,
+			},
+			1000: {
+				items: 1,
+			},
+		},
+	});
+});
